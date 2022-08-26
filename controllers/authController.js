d627fa3c95f86c980b46c7ce0cf3808b3b11929b
Lucas Faustman <lucasfaustman@gmail.com>
2022-08-25 20:25:09 -0700

    Fixed capitalization

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,12 +41,12 @@ const createToken = (id) => {
 }
 
 module.exports.signup_get = (req,res) => {
-    res.render('signup', { title: 'Sign Up', layout: "layout/layout-WithoutNav" });
+    res.render('signup', { title: 'Sign Up', layout: "Layout/Layout-withoutNav" });
                                                             
 }
 
 module.exports.login_get = (req,res) => {
-    res.render('login', { title: 'Sign In', layout: "layout/layout-WithoutNav" });
+    res.render('login', { title: 'Sign In', layout: "Layout/Layout-withoutNav" });
 }
 
 module.exports.signup_post = async (req,res) => {
